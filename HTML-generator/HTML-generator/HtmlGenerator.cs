276187260f3b840f18d5aca42033991a960e5064
Stoma Fedor <fedorstoma2@gmail.com>
2023-09-29 00:14:29 +0300

    generate datetime and repos

---
+++ HTML-generator/HTML-generator/HtmlGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Aspose.Html;
+
+namespace HTML_generator
+{
+    public class HtmlGenerator
+    {
+        public HtmlGenerator()
+        {
+            Document = new HTMLDocument();
+            CreateHeader("h2", "Gitchecker");
+        }
+
+        public HTMLDocument Document { get; set; }
+
+        private void CreateHeader(string headerType, string headerText)
+        {
+            var h2 = (HTMLHeadingElement)Document.CreateElement(headerType);
+            var text = Document.CreateTextNode(headerText);
+            h2.AppendChild(text);
+            Document.Body.AppendChild(h2);
+        }
+
+        private void CreateParagraph(string parText)
+        {
+            var paragraph = (HTMLParagraphElement)Document.CreateElement("p");
+            var text = Document.CreateTextNode(parText);
+            paragraph.AppendChild(text);
+            Document.Body.AppendChild(paragraph);
+        }
+
+        private void CreateDatetime()
+        {
+            var paragraph = (HTMLParagraphElement)Document.CreateElement("p");
+            var data = DateTime.UtcNow.ToString();
+            var text = Document.CreateTextNode(data);
+            paragraph.AppendChild(text);
+            Document.Body.AppendChild(paragraph);
+        }
+
+        private void CreateRepositories()
+        {
+            CreateParagraph("Количество репозиториев: " + ""); //добавить количество
+
+
+            var paragraph = (HTMLParagraphElement)Document.CreateElement("p");
+            var data = "Дата и время прогона антиплагиата: " + DateTime.UtcNow;
+            var text = Document.CreateTextNode(data);
+            paragraph.AppendChild(text);
+            Document.Body.AppendChild(paragraph);
+        }
+    }
+}
---
+++ HTML-generator/HTML-generator/Program.cs
@@ -0,0 +1,36 @@
+﻿using Aspose.Html;
+
+var document = new HTMLDocument();
+
+var h2 = (HTMLHeadingElement)document.CreateElement("h2");
+var text = document.CreateTextNode("gitchecker");
+h2.AppendChild(text);
+document.Body.AppendChild(h2);
+
+var p = (HTMLParagraphElement)document.CreateElement("p");
+var data = "Дата и время прогона антиплагиата: " + DateTime.UtcNow;
+var paraText = document.CreateTextNode(data);
+p.AppendChild(paraText);
+document.Body.AppendChild(p);
+
+// Добавить упорядоченный список
+// Создать элемент абзаца
+var list = (HTMLOListElement)document.CreateElement("ol");
+
+// Добавить элемент 1
+var item1 = (HTMLLIElement)document.CreateElement("li");
+item1.AppendChild(document.CreateTextNode("First list item."));
+
+// Добавить пункт 2
+var item2 = (HTMLLIElement)document.CreateElement("li");
+item2.AppendChild(document.CreateTextNode("Second list item."));
+
+// Добавить li элементы в список
+list.AppendChild(item1);
+list.AppendChild(item2);
+
+// Прикрепить список к телу документа 
+document.Body.AppendChild(list);
+
+// Сохраните документ HTML в файл 
+document.Save(@"C:\Users\100ma\Desktop\Fedor100ma\Стажировка\create-new-document.html");
