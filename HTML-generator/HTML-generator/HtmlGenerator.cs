5cef1769e353fbf62ff8912694f4b3fed5581a14
Stoma Fedor <fedorstoma2@gmail.com>
2023-09-29 04:33:37 +0300

    add table generate

--- HTML-generator/HTML-generator/HtmlGenerator.cs
+++ HTML-generator/HTML-generator/HtmlGenerator.cs
@@ -7,10 +7,19 @@ namespace HTML_generator
         public HtmlGenerator()
         {
             Document = new HTMLDocument();
-            CreateHeader("h2", "Gitchecker");
         }
 
         public HTMLDocument Document { get; set; }
+
+        public void GenerateHTML(List<string> repoLinks, List<List<string>> matchMatrix)
+        {
+            CreateHeader("h2", "Gitchecker");
+            CreateDatetime();
+            CreateRepositories(repoLinks);
+            CreateRepoTable(matchMatrix);
+            var downloads = Syroot.Windows.IO.KnownFolders.Downloads.Path;
+            Document.Save($"{downloads}/gitchecker.html");
+        }
 
         private void CreateHeader(string headerType, string headerText)
         {
@@ -30,23 +39,52 @@ namespace HTML_generator
 
         private void CreateDatetime()
         {
+            CreateHeader("h3", "Дата и время");
             var paragraph = (HTMLParagraphElement)Document.CreateElement("p");
-            var data = DateTime.UtcNow.ToString();
-            var text = Document.CreateTextNode(data);
+            var text = Document.CreateTextNode(DateTime.Now.ToString());
             paragraph.AppendChild(text);
             Document.Body.AppendChild(paragraph);
         }
 
-        private void CreateRepositories()
+        private void CreateRepositories(List<string> repoLinks)
         {
-            CreateParagraph("Количество репозиториев: " + ""); //добавить количество
+            CreateHeader("h3", "Репозитории");
+            CreateParagraph($"Количество: {repoLinks.Count}");
 
+            var list = (HTMLOListElement)Document.CreateElement("ol");
 
-            var paragraph = (HTMLParagraphElement)Document.CreateElement("p");
-            var data = "Дата и время прогона антиплагиата: " + DateTime.UtcNow;
-            var text = Document.CreateTextNode(data);
-            paragraph.AppendChild(text);
-            Document.Body.AppendChild(paragraph);
+            foreach (string repoLink in repoLinks)
+            {
+                var item = (HTMLLIElement)Document.CreateElement("li");
+                item.AppendChild(Document.CreateTextNode(repoLink));
+                list.AppendChild(item);
+            }
+            Document.Body.AppendChild(list);
+        }
+
+        private void CreateRepoTable(List<List<string>> matchMatrix)
+        {
+            CreateHeader("h3", "Результаты проверки репозиториев");
+            var htmlTable = "<table style=\"border-collapse: collapse;\"><tbody><tr>";
+            htmlTable += "<th scope=\"col\">repo num</th>";
+            for (int i = 1; i < matchMatrix.Count + 1; i++)
+            {
+                htmlTable += $"<th scope=\"col\" style=\"border: 1px solid black;\">{i}</th>";
+            }
+            htmlTable += "</tr>";
+            var count = 1;
+            foreach (var match in matchMatrix)
+            {
+                htmlTable += $"<tr><th scope=\"row\" style=\"border: 1px solid black;\">{count}</th>";
+                foreach (var item in match)
+                {
+                    htmlTable += $"<td style=\"border: 1px solid black;\">{item}</td>";
+                }
+                count++;
+                htmlTable += "</tr>";
+            }
+            htmlTable += "</tbody></table>";
+            Document.Body.InnerHTML += htmlTable;
         }
     }
 }
--- HTML-generator/HTML-generator/Program.cs
+++ HTML-generator/HTML-generator/Program.cs
@@ -1,36 +1,11 @@
-﻿using Aspose.Html;
+﻿using HTML_generator;
 
-var document = new HTMLDocument();
-
-var h2 = (HTMLHeadingElement)document.CreateElement("h2");
-var text = document.CreateTextNode("gitchecker");
-h2.AppendChild(text);
-document.Body.AppendChild(h2);
-
-var p = (HTMLParagraphElement)document.CreateElement("p");
-var data = "Дата и время прогона антиплагиата: " + DateTime.UtcNow;
-var paraText = document.CreateTextNode(data);
-p.AppendChild(paraText);
-document.Body.AppendChild(p);
-
-// Добавить упорядоченный список
-// Создать элемент абзаца
-var list = (HTMLOListElement)document.CreateElement("ol");
-
-// Добавить элемент 1
-var item1 = (HTMLLIElement)document.CreateElement("li");
-item1.AppendChild(document.CreateTextNode("First list item."));
-
-// Добавить пункт 2
-var item2 = (HTMLLIElement)document.CreateElement("li");
-item2.AppendChild(document.CreateTextNode("Second list item."));
-
-// Добавить li элементы в список
-list.AppendChild(item1);
-list.AppendChild(item2);
-
-// Прикрепить список к телу документа 
-document.Body.AppendChild(list);
-
-// Сохраните документ HTML в файл 
-document.Save(@"C:\Users\100ma\Desktop\Fedor100ma\Стажировка\create-new-document.html");
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        HtmlGenerator htmlGenerator = new HtmlGenerator();
+        var a = new List<string>() { "repo1", "repo2", "repo3" };
+        htmlGenerator.GenerateHTML(a, new List<List<string>>() { new List<string>() { "100", "44", "77" }, new List<string>() { "44", "100", "77" }, new List<string>() { "77", "44", "100" } });
+    }
+}
